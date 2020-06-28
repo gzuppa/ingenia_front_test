dcb051eab1717ed0c7ad24c614ba97b092484bab
Gustavo Zuppa <gustavo.zuppa@ulfix.com>
2020-06-27 21:25:47 -0500

    widths

--- ingenia-front-test/src/components/Cover/Cover.js
+++ ingenia-front-test/src/components/Cover/Cover.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col} from 'react-bootstrap'
+import { Container, Row, Col, Card} from 'react-bootstrap'
 import Layout from '../../assets/Layout'
 import '../components.css'
 
@@ -10,10 +10,31 @@ class Cover extends React.Component {
         <Container>
         <Row>
           <Col className="coverMexico">
-            <img src={Layout.coverImgs.coverMexico} alt='mexico' />
+            <img src={Layout.coverImgs.coverMexico} alt='mexico' /> 
+            <h2>Mexico City</h2>
+            <Card className="cardMexico" style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Anatole France 311</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Polanco, Miguel Hidalgo, 11560, Mexico DF</Card.Subtitle>
+                            <Card.Text>
+                                50 22 09 00
+                            </Card.Text>
+                </Card.Body>
+            </Card>
           </Col>
           <Col className="coverWashington">
-            <img src={Layout.coverImgs.coverWashington} alt='washington' />
+            <img className="imgwsh" src={Layout.coverImgs.coverWashington} alt='washington' />
+            <h2>Washington D.C.</h2>
+            <Card className="cardWashington" style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>1200 18th Street, NW</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Suite 700</Card.Subtitle>
+                        <Card.Subtitle className="mb-2 text-muted">Washington DC 20036 USA</Card.Subtitle>
+                            <Card.Text>
+                                50 22 09 00
+                            </Card.Text>
+                </Card.Body>
+            </Card>
           </Col>
         </Row>
       </Container>
